6eb95b17b883897b371874c1953c2a13409e1fc0
Carina <revuwem@gmail.com>
2022-08-30 23:54:56 +0400

    feat: add basic thorus geometry

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6376,
+  wireframe: true,
+});
+const thorus = new THREE.Mesh(geometry, material);
+
+scene.add(thorus);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  thorus.rotation.x += 0.01;
+  thorus.rotation.y += 0.005;
+  thorus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
